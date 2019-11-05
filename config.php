5480b975c9c5c2e4294e742163a3d442c930a668
Pavel E. Petrov <9624391+far-rainbow@users.noreply.github.com>
2019-11-05 19:49:26 +0300

    Update config.php

--- config.php
+++ config.php
@@ -6,35 +6,20 @@ define('TRANS', false);
 $servers = [
     [
         'type' => 'mysql',
-        'host' => '192.168.1.93',
-        'dbname' => 'test',
-        'user' => 'admin',
-        'password' => 'bpumAhpmyMrMxPNY',
-        'enabled' => true
-    ],
-    [
-        'type' => 'mysql',
-        'host' => '127.0.0.1',
-        'dbname' => 'test2',
-        'user' => 'test',
-        'password' => 'CBzLLRDsxAYHJLSs',
-        'enabled' => true
-    ],
-    [
-        'type' => 'mysql',
         'host' => '88.99.112.21',
         'dbname' => 'test',
         'user' => 'test',
         'password' => 'pp8kgvZQh4b60jWg',
-        'enabled' => false
+        'enabled' => true
     ],
     [
         'type' => 'pgsql',
-        'host' => '192.168.1.93',
+        'host' => '95.216.144.125',
         'dbname' => 'test',
         'user' => 'test',
         'password' => 'pp8kgvZQh4b60jWg',
         'enabled' => true
-    ]
+    ],
+
     
 ];
